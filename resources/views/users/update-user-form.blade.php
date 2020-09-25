8a0f3e8b710d73c4f2d6a6061fa1a2e533c80527
Oliver Jiménez Servín <oliverds@users.noreply.github.com>
2020-09-24 19:42:33 -0500

    wip

--- resources/views/users/update-user-form.blade.php
+++ resources/views/users/update-user-form.blade.php
@@ -57,39 +57,6 @@
             <x-mkt-input id="email" type="email" class="mt-1 block w-full" wire:model.defer="state.email" />
             <x-mkt-input-error for="email" class="mt-2" />
         </div>
-
-        <!-- Role -->
-        @if (count($this->roles) > 0)
-            <div class="col-span-6 lg:col-span-4">
-                <x-mkt-label for="role" value="{{ __('Role') }}" />
-                <x-mkt-input-error for="role" class="mt-2" />
-
-                <div class="mt-1 border border-gray-200 rounded-lg cursor-pointer">
-                    @foreach ($this->roles as $index => $role)
-                            <div class="px-4 py-3 {{ $index > 0 ? 'border-t border-gray-200' : '' }}"
-                                            wire:click="$set('state.role', '{{ $role->key }}')">
-                                <div class="{{ isset($state['role']) && $state['role'] !== $role->key ? 'opacity-50' : '' }}">
-                                    <!-- Role Name -->
-                                    <div class="flex items-center">
-                                        <div class="text-sm text-gray-600 {{ $state['role'] == $role->key ? 'font-semibold' : '' }}">
-                                            {{ $role->name }}
-                                        </div>
-
-                                        @if ($state['role'] == $role->key)
-                                            <svg class="ml-2 h-5 w-5 text-green-400" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                        @endif
-                                    </div>
-
-                                    <!-- Role Description -->
-                                    <div class="mt-2 text-xs text-gray-600">
-                                        {{ $role->description }}
-                                    </div>
-                                </div>
-                            </div>
-                    @endforeach
-                </div>
-            </div>
-        @endif
     </x-slot>
 
     <x-slot name="actions">
---
+++ src/Traits/HasProfilePhoto.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Marketplaceful\Traits;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+trait HasProfilePhoto
+{
+    public function updateProfilePhoto(UploadedFile $photo)
+    {
+        tap($this->profile_photo_path, function ($previous) use ($photo) {
+            $this->forceFill([
+                'profile_photo_path' => $photo->storePublicly(
+                    'profile-photos',
+                    ['disk' => $this->profilePhotoDisk()]
+                ),
+            ])->save();
+
+            if ($previous) {
+                Storage::disk($this->profilePhotoDisk())->delete($previous);
+            }
+        });
+    }
+
+    public function deleteProfilePhoto()
+    {
+        Storage::disk($this->profilePhotoDisk())->delete($this->profile_photo_path);
+
+        $this->forceFill([
+            'profile_photo_path' => null,
+        ])->save();
+    }
+
+    public function getProfilePhotoUrlAttribute()
+    {
+        return $this->profile_photo_path
+                    ? Storage::disk($this->profilePhotoDisk())->url($this->profile_photo_path)
+                    : $this->defaultProfilePhotoUrl();
+    }
+
+    protected function defaultProfilePhotoUrl()
+    {
+        return 'https://ui-avatars.com/api/?name='.urlencode($this->name).'&color=7F9CF5&background=EBF4FF';
+    }
+
+    protected function profilePhotoDisk()
+    {
+        return isset($_ENV['VAPOR_ARTIFACT_NAME']) ? 's3' : 'public';
+    }
+}
