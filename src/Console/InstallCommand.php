bb60a2ad1301eda45099f8232116ae617b0bf923
Oliver Jiménez Servín <oliverds@users.noreply.github.com>
2020-10-08 19:40:11 -0500

    wip

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -13,7 +13,7 @@ class InstallCommand extends Command
     public function handle()
     {
         // Publish...
-        $this->callSilent('vendor:publish', ['--tag' => 'marketplaceful-config', '--force' => true]);
+        $this->callSilent('vendor:publish', ['--tag' => 'marketplaceful-config']);
         $this->callSilent('vendor:publish', ['--tag' => 'marketplaceful-migrations', '--force' => true]);
         $this->callSilent('vendor:publish', ['--tag' => 'marketplaceful-dashboard', '--force' => true]);
 
