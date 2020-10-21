8c64db46eb64556812d146c84cfe47bdef64517c
Oliver Jiménez Servín <oliverds@users.noreply.github.com>
2020-10-21 18:42:50 -0500

    wip

---
+++ database/migrations/2020_10_02_003358_create_conversations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConversationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->unsignedBigInteger('listing_id')->index();
+            $table->timestamp('last_message_at');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversations');
+    }
+}
